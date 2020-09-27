4adc36ae55650f7541623ccee818cfe5d1d9f355
doyledyp <doyledev@protonmail.com>
2020-09-27 20:13:37 +0100

    Theme Workout

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -335,7 +335,7 @@ export default function WalletModal({
           </HeaderRow>
         ) : (
           <HeaderRow>
-            <HoverText>Connect to a wallet</HoverText>
+            <HoverText>Connect your wallet</HoverText>
           </HeaderRow>
         )}
         <ContentWrapper>
--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -5,6 +5,7 @@ export interface Colors {
   // base
   white: Color
   black: Color
+  red: Color
 
   // text
   text1: Color
