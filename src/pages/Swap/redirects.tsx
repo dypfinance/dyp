7c6277c5b691a3c6ed58656ee94f1f2e70ed278c
doyledyp <doyledev@protonmail.com>
2020-09-30 16:59:10 +0100

    Redirect to Home

--- src/pages/Swap/redirects.tsx
+++ src/pages/Swap/redirects.tsx
@@ -6,7 +6,7 @@ import { ApplicationModal, setOpenModal } from '../../state/application/actions'
 
 // Redirects to swap but only replace the pathname
 export function RedirectPathToSwapOnly({ location }: RouteComponentProps) {
-  return <Redirect to={{ ...location, pathname: '/swap' }} />
+  return <Redirect to={{ ...location, pathname: '/home' }} />
 }
 
 // Redirects from the /swap/:outputCurrency path to the /swap?outputCurrency=:outputCurrency format
@@ -22,7 +22,7 @@ export function RedirectToSwap(props: RouteComponentProps<{ outputCurrency: stri
     <Redirect
       to={{
         ...props.location,
-        pathname: '/swap',
+        pathname: '/home',
         search:
           search && search.length > 1
             ? `${search}&outputCurrency=${outputCurrency}`
