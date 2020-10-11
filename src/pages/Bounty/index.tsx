8eab28f36c73e02007944b3a8b1d28322d6f9e40
doyledyp <doyledev@protonmail.com>
2020-10-11 19:12:11 +0100

    Bounty & Some Fixes

--- src/pages/Bounty/index.tsx
+++ src/pages/Bounty/index.tsx
@@ -278,9 +278,9 @@ export default function Bounty() {
                   </Link>
                   {/* eslint-disable-next-line react/no-unescaped-entities */}, you are not allowed to add members with
                   "Add members" function because this is considered Spam by telegram. You need to have at least 5
-                  members from the invited ones to post constructive messages in our group. If your members leave the
+                  members from the invited ones to post constructive messages in our group. If your members leave the{' '}
                   {/* eslint-disable-next-line react/no-unescaped-entities */}
-                  DYP's Official telegram group before the campaign ends, your stakes will be deleted.
+                  DYP's Official telegram group before the campaign ends, your stakes will be deleted
                 </TYPE.white>
               </RowBetween>
               <RowBetween>
@@ -347,10 +347,10 @@ export default function Bounty() {
                 <TYPE.white fontSize={14}>
                   {/* eslint-disable-next-line react/no-unescaped-entities */}
                   3. Invite at least 10 new members to DYP's Official discord channel. The new members must join our
-                  channel by the invite link that you generate with your username, under DeFi Yield Protocol server name
+                  channel by the invite link that you generate with your username, under DeFi Yield Protocol server name{' '}
                   {/* eslint-disable-next-line react/no-unescaped-entities */}
                   you click "Invite" and you get a "Copy Share Link", please note that the link expires in 1 day, so
-                  every 24 hours you need to generate a new one. You need to have at least 5 members from the invited
+                  every 24 hours you need to generate a new one. You need to have at least 5 members from the invited{' '}
                   {/* eslint-disable-next-line react/no-unescaped-entities */}
                   ones to post constructive messages in our channel. If your members leave the DYP's Official discord
                   channel before the campaign ends, your stakes will be deleted.
