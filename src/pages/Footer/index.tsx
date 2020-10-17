de4a59810491a759bad5f4730f980a9f2e9bdd2f
doyledyp <doyledev@protonmail.com>
2020-10-17 05:27:32 +0100

    Footer Mobile

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -10,6 +10,63 @@ import Ann from '../../assets/svg/ann.png'
 import Email from '../../assets/svg/email.svg'
 import Forum from '../../assets/svg/forum.svg'
 
+const StyledFooter = styled.footer`
+  background-color: #1e272f;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+const StyledFooterInner = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // height: 20px;
+  max-width: 1200px;
+  width: 100%;
+`
+
+const StyledNav = styled.nav`
+  align-items: center;
+  display: flex;
+  -webkit-align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`
+
+const StyledLink = styled.a`
+  color: grey;
+  margin: 30px 15px;
+  -webkit-align-items: center;
+  text-decoration: none;
+  &:hover {
+    color: grey;
+  }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+        margin: 5px 12px;
+  `}
+`
+const StyledLinkS = styled.div`
+  color: grey;
+  margin: 30px 15px;
+  -webkit-align-items: center;
+  text-decoration: none;
+  &:hover {
+    color: grey;
+  }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+        margin: 5px 12px;
+  `}
+`
+
+// const StyledSpace = styled.div`
+//   width: 100px;
+//   margin: 5px auto;
+//   max-width: 100%;
+//   background-color: rgba(0, 0, 0, 0);
+// `
+
 const Footer: React.FC = () => (
   <StyledFooter>
     <StyledFooterInner>
@@ -53,56 +110,5 @@ const Footer: React.FC = () => (
     </StyledFooterInner>
   </StyledFooter>
 )
-
-const StyledFooter = styled.footer`
-  background-color: #1e272f;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`
-const StyledFooterInner = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  // height: 20px;
-  max-width: 1200px;
-  width: 100%;
-`
-
-const StyledNav = styled.nav`
-  align-items: center;
-  display: flex;
-  -webkit-align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
-`
-
-const StyledLink = styled.a`
-  color: grey;
-  margin: 30px 15px;
-  -webkit-align-items: center;
-  text-decoration: none;
-  &:hover {
-    color: grey;
-  }
-`
-const StyledLinkS = styled.div`
-  color: grey;
-  margin: 30px 15px;
-  -webkit-align-items: center;
-  text-decoration: none;
-  &:hover {
-    color: grey;
-  }
-`
-
-// const StyledSpace = styled.div`
-//   width: 100px;
-//   margin: 5px auto;
-//   max-width: 100%;
-//   background-color: rgba(0, 0, 0, 0);
-// `
 
 export default Footer
