264dcdfd942d954330c675b845858e1af820eb99
doyledyp <doyledev@protonmail.com>
2020-10-24 05:53:51 +0100

    Remove App DarkMode

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,6 +10,7 @@ import Popups from '../components/Popups'
 import Web3ReactManager from '../components/Web3ReactManager'
 import { ApplicationModal } from '../state/application/actions'
 import { useModalOpen, useToggleModal } from '../state/application/hooks'
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
 import AddLiquidity from './AddLiquidity'
 import {
   RedirectDuplicateTokenIds,
@@ -96,6 +97,7 @@ export default function App() {
   return (
     <Suspense fallback={null}>
       <Route component={GoogleAnalyticsReporter} />
+      <Route component={DarkModeQueryParamReader} />
       <AppWrapper>
         <URLWarning />
         <HeaderWrapper>
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -43,7 +43,7 @@ export function colors(darkMode: boolean): Colors {
     red,
 
     // text
-    text1: darkMode ? '#FFFFFF' : '#000000',
+    text1: darkMode ? '#000000' : '#000000',
     text2: darkMode ? '#C3C5CB' : '#565A69',
     text3: darkMode ? '#6C7284' : '#888D9B',
     text4: darkMode ? '#565A69' : '#C3C5CB',
@@ -51,7 +51,7 @@ export function colors(darkMode: boolean): Colors {
 
     // backgrounds / greys
     bg1: darkMode ? '#212429' : '#FFFFFF',
-    bg2: darkMode ? '#2C2F36' : 'white',
+    bg2: darkMode ? '#FFFFFF' : '#FFFFFF',
     bg3: darkMode ? '#40444F' : '#EDEEF2',
     bg4: darkMode ? '#565A69' : '#CED0D9',
     bg5: darkMode ? '#6C7284' : '#888D9B',
