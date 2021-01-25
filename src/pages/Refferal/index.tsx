302e0e71b7b49e69b7169cc6fa119c9a95e9db60
doyledyp <doyledev@protonmail.com>
2021-01-25 22:28:11 +0000

    Update Referral

--- src/pages/Refferal/index.tsx
+++ src/pages/Refferal/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { TYPE } from '../../theme'
 import { RowBetween } from '../../components/Row'
 import { CardSection, DataCard, CardNoise, CardBGImage } from '../../components/earn/styled'
-import LOGO from '../../assets/images/soon.png'
+//import LOGO from '../../assets/images/soon.png'
 import Whitelist from '../../components/Whitelist'
 
 const PageWrapper = styled(AutoColumn)`
@@ -15,6 +15,15 @@ const PageWrapper = styled(AutoColumn)`
 const TopSection = styled(AutoColumn)`
   max-width: 720px;
   width: 100%;
+`
+
+const StyledLink = styled.a`
+  color: grey;
+  -webkit-align-items: center;
+  text-decoration: none;
+  &:hover {
+    color: grey;
+  }
 `
 
 export default function Refferal() {
@@ -29,15 +38,28 @@ export default function Refferal() {
             <AutoColumn gap="md">
               <RowBetween>
                 <TYPE.white fontWeight={600}>DYP Referral</TYPE.white>
-                <img src={LOGO} style={{ height: '39px' }} alt={'logo'} />
+                {/*<img src={LOGO} style={{ height: '39px' }} alt={'logo'} />*/}
               </RowBetween>
               <RowBetween>
                 <TYPE.white fontSize={14}>
-                  Should you refer DYP to your friends, 5% of your friends’ rewards will be automatically sent to you
+                  Should you refer DYP to your friends, 5% of your friends rewards will be automatically sent to you
                   whenever your friends stake DYP. You do not need to stake, it’s automatically sent to you, free of gas
                   fee.
                 </TYPE.white>
               </RowBetween>
+              <RowBetween>
+                <TYPE.white fontSize={14}>
+                  Visit{' '}
+                  <StyledLink
+                    target="_blank"
+                    style={{ color: 'white', textDecoration: 'underline' }}
+                    href="https://app.dyp.finance/constant-staking-30"
+                  >
+                    app.dyp.finance/constant-staking-30
+                  </StyledLink>{' '}
+                  to get your Referral link.
+                </TYPE.white>
+              </RowBetween>{' '}
             </AutoColumn>
           </CardSection>
           <CardBGImage />
