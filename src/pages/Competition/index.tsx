264f0ee79574ab3d2fc3caae6897621f1ebfc437
doyledyp <doyledev@protonmail.com>
2021-03-01 14:18:02 +0000

    Update Competition Page

--- src/pages/Competition/index.tsx
+++ src/pages/Competition/index.tsx
@@ -3,7 +3,7 @@ import { AutoColumn } from '../../components/Column'
 import styled from 'styled-components'
 import { TYPE } from '../../theme'
 import { RowBetween } from '../../components/Row'
-import { CardSection, DataCard, CardNoise, CardBGImage } from '../../components/earn/styled'
+import { CardSection } from '../../components/earn/styled'
 import Whitelist from '../../components/Whitelist'
 
 import Banner from '../../assets/images/dyp_giveaway.jpg'
@@ -36,71 +36,65 @@ export default function Competition() {
     <PageWrapper gap="lg" justify="center">
       <Whitelist />
       <TopSection gap="md">
-        <DataCard>
-          <CardBGImage />
-          <CardNoise />
-          <CardSection>
-            <AutoColumn gap="md">
-              <RowBetween>
-                <TYPE.white fontWeight={600} fontSize={15}>
-                  DeFi Yield Protocol Buying Competition: $25,000 in USDT to be won!
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <img src={Banner} style={{ width: '100%', borderRadius: '25px' }} alt={'logo'} />
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>
-                  DeFi Yield Protocol team has committed a total of $25,000 in USDT to be given away as prizes to reward
-                  our new users.
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>
-                  The campaign will run from 18:00 on March 01, 2021 to 18:00 on March 30, 2021 (UTC).
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontWeight={600} fontSize={15}>
-                  New 100 Token Holders: Win a Share of $25,000 USDT!
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>
-                  The top 50 new token holders will win a share of $15,000 USDT rewards in proportion to their holding
-                  amount.<div></div>
-                  Reward for top 50= the top 50 token holders from March 01, 2021 to March 30, 2021 will share $15,000
-                  USDT based on their share of DYP holding.
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>
-                  50 new token holders will share $10,000 USDT in 200 prizes of $200 in USDT.<div></div>
-                  Reward for 50 new token holders= 50 random token holders that bough DYP between March 01, 2021 and
-                  March 30, 2021 will get each $200 in USDT.
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={15} fontWeight={600}>
-                  Note:
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>
-                  1. The campaign is only for the token holders that bought DYP between March 01, 2021 and March 30,
-                  2021.<div></div>
-                  2. The minimum amount of DYP that you need to hold is 5 DYP tokens.<div></div>
-                  3. The Sub-Holders (addresses that received DYP tokens from an existent holder) will be treated as the
-                  same account when participating in the activity.<div></div>
-                  4. The final rankings will be updated within 3 days of the competition ending.<div></div>
-                  5. Rewards will be distributed within 7 days of the campaign ending.
-                </TYPE.white>
-              </RowBetween>
-            </AutoColumn>
-          </CardSection>
-          <CardBGImage />
-          <CardNoise />
-        </DataCard>
+        <CardSection>
+          <AutoColumn gap="md">
+            <RowBetween>
+              <TYPE.black fontWeight={600} fontSize={15}>
+                DeFi Yield Protocol Buying Competition: $25,000 in USDT to be won!
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <img src={Banner} style={{ width: '100%', borderRadius: '25px' }} alt={'logo'} />
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={14}>
+                DeFi Yield Protocol team has committed a total of $25,000 in USDT to be given away as prizes to reward
+                our new users.
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={14}>
+                The campaign will run from 18:00 on March 01, 2021 to 18:00 on March 30, 2021 (UTC).
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontWeight={600} fontSize={15}>
+                New 100 Token Holders: Win a Share of $25,000 USDT!
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={14}>
+                The top 50 new token holders will win a share of $15,000 USDT rewards in proportion to their holding
+                amount.<div></div>
+                Reward for top 50= the top 50 token holders from March 01, 2021 to March 30, 2021 will share $15,000
+                USDT based on their share of DYP holding.
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={14}>
+                50 new token holders will share $10,000 USDT in 200 prizes of $200 in USDT.<div></div>
+                Reward for 50 new token holders= 50 random token holders that bough DYP between March 01, 2021 and March
+                30, 2021 will get each $200 in USDT.
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={15} fontWeight={600}>
+                Note:
+              </TYPE.black>
+            </RowBetween>
+            <RowBetween>
+              <TYPE.black fontSize={14}>
+                1. The campaign is only for the token holders that bought DYP between March 01, 2021 and March 30, 2021.
+                <div></div>
+                2. The minimum amount of DYP that you need to hold is 5 DYP tokens.<div></div>
+                3. The Sub-Holders (addresses that received DYP tokens from an existent holder) will be treated as the
+                same account when participating in the activity.<div></div>
+                4. The final rankings will be updated within 3 days of the competition ending.<div></div>
+                5. Rewards will be distributed within 7 days of the campaign ending.
+              </TYPE.black>
+            </RowBetween>
+          </AutoColumn>
+        </CardSection>
       </TopSection>
     </PageWrapper>
   )
