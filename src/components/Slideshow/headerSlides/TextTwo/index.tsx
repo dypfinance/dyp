54e9e3f119c7eadf720a78f045c397b5ebcd1209
doyledyp <doyledev@protonmail.com>
2021-03-14 21:51:25 +0000

    Update Whitelist Slideshow Text

--- src/components/Slideshow/headerSlides/TextTwo/index.tsx
+++ src/components/Slideshow/headerSlides/TextTwo/index.tsx
@@ -87,7 +87,7 @@ export default function TextTwo() {
     <SContainer>
       <TYPE.white fontWeight={400} fontSize={17} style={{ margin: 'auto', textDecoration: 'underline' }}>
         <TextFont>
-          {getFormattedNumber(wethPaiOutTotals, 2)} ETH Ethereum worth ${getFormattedNumber(paidInUsd, 2)} paid to the
+          {getFormattedNumber(wethPaiOutTotals, 2)} Ethereum worth ${getFormattedNumber(paidInUsd, 2)} paid to the
           Liquidity providers since launch
         </TextFont>
       </TYPE.white>
