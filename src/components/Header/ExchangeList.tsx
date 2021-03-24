9205095879e48dd96f89cc61440b28c708c85e82
doyledyp <doyledev@protonmail.com>
2021-03-24 09:38:35 +0000

    Update Header + Home

--- src/components/Header/ExchangeList.tsx
+++ src/components/Header/ExchangeList.tsx
@@ -34,7 +34,7 @@ export default function ExchangeList() {
     <PhishAlert isActive={true}>
       <div style={{ display: 'flex' }}>
         <img src={Kucoin} width={30} height={30} style={{ margin: '10px', marginLeft: '0px' }} alt={'logo'} />
-        <code style={{ marginTop: '17px', display: 'inline', fontWeight: 'bold' }}>
+        <code style={{ marginTop: '19px', display: 'inline', fontWeight: 'bold' }}>
           DeFi Yield Protocol (DYP) GETS LISTED ON KuCoin
         </code>
       </div>
